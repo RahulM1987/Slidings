414c85c9562c1d8cd812332a906abfa67e4c18f9
Rahul Morade <rahul@maxdigi.com>
2014-08-20 10:56:41 +0530

    Initial Commit

---
+++ Slidings/MainAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MainAppDelegate.h
+//  Slidings
+//
+//  Created by Rahul on 20/08/14.
+//  Copyright (c) 2014 Rahul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
